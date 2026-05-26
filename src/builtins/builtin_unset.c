9609d00ed8b03d69e9347740be5d2170071ec450
VB-Commits <valentin.bisco@gmail.com>
2026-05-26 18:12:32 +0200

    - fixed bi_unset after testing edge case (export t=t=, unset t=t= should not work)

--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: vabisco <vabisco@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 17:04:00 by vabisco           #+#    #+#             */
-/*   Updated: 2026/05/26 17:10:43 by vabisco          ###   ########.fr       */
+/*   Updated: 2026/05/26 18:10:44 by vabisco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,15 @@ int	builtin_unset(t_ctx *ctx, char **args)
 		i = 0;
 		while (ctx->envp[i])
 		{
-			name_len = ft_strlen(*args);
+			name_len = ft_strclen(ctx->envp[i], '=');
 			if (ft_strncmp(ctx->envp[i], *args, name_len) == 0
 				&& (ctx->envp[i])[name_len] == '=' && (*args)[name_len] == '\0')
 				break;
 			i++;
 		}
-		/*debug*/ ft_printf("pos=%s[%zu]\n", ctx->envp[i], i);
-		//breakpoint
 		if (ctx->envp[i])
 		{
 			envp_size = ft_arrlen((void **)ctx->envp);
-			/*debug*/ ft_printf("envp+size=%zu\n", envp_size);
 			free(ctx->envp[i]);
 			ft_memmove(ctx->envp + i, ctx->envp + i + 1, (envp_size - i) * sizeof(char *));
 			ctx->envp = ft_realloc(ctx->envp, (envp_size + 1) * sizeof(char *), (envp_size - 1 + 1) * sizeof(char *));
